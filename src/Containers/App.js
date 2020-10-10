1efca545a74f2fba3420ee00f2c762690a98ec9f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-10 19:41:03 -0400

    created simple .css file for header; rearanged file tree

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../Styling/App.css';
 import Header from './Header.js'
 
 
--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import Subheader from './Subheader'
+import Subheader from '../Components/Subheader'
+import '../Styling/Header.css'
 
 class Header extends React.Component {
+    
+    
 
     philosophers = () => {
     return this.props.philList.map(function (philosopher) {return <Subheader name={philosopher} />} )
@@ -10,8 +13,8 @@ class Header extends React.Component {
     render() {
         return(
             <div>
-                <h1> Welcome to A Dorky React Practice Project </h1>
-                <h2>List of Philosophers: </h2>
+                <h1 className="dorky-header"> Welcome to A Dorky React Practice Project </h1>
+                <h2 className="phil-list-header">List of Philosophers: </h2>
                 {this.philosophers()}
              </div>
         )
